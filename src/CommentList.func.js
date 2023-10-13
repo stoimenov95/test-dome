0e45045ac28968b9b3d15767f4d35139de231659
Antonio <32269810+stoimenov95@users.noreply.github.com>
2023-10-13 01:13:37 +0000

    Local setup.

---
+++ src/CommentList.func.js
@@ -0,0 +1,53 @@
+const CommentList = () => {
+    const [comments, setComments] = React.useState([]);
+    const [currentComment, setCurrentComment] = React.useState('');
+
+    const handleInputChange = (event) => {
+        setCurrentComment(event.target.value);
+    }
+
+    const handlePostClick = () => {
+        if (currentComment.trim() !== '') {
+            setComments(prevComments => [...prevComments, currentComment]);
+            setCurrentComment('');
+        }
+    }
+
+    return (
+        <div>
+            <form>
+                <input 
+                    type="text" 
+                    value={currentComment} 
+                    onChange={handleInputChange}
+                />
+                <input 
+                    type="button" 
+                    value="Post" 
+                    onClick={handlePostClick}
+                />
+            </form>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+document.body.innerHTML = "<div id='root'></div>";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<CommentList />);
+
+setTimeout(() => {
+    let input = document.querySelector("input[type='text']");
+    input.value = "test";
+    input.dispatchEvent(new Event('change', { bubbles: true }));
+    
+    setTimeout(() => {
+        document.querySelector("input[type='button']").click();
+        console.log(document.getElementsByTagName("ul")[0].innerHTML);
+    });
+});
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,50 +1,39 @@
-// React is loaded and is available as React and ReactDOM
-// imports should NOT be used
+const CommentList = () => {
+  const [comments, setComments] = React.useState([]);
+  const [currentComment, setCurrentComment] = React.useState('');
 
-import React from "react";
-import ReactDOM from "react-dom";
-
-class CommentList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: [],
-      currentComment: ""
-    };
+  const handleInputChange = (event) => {
+      setCurrentComment(event.target.value);
   }
 
-  handleInputChange = (event) => {
-    this.setState({ currentComment: event.target.value });
-  };
+  const handlePostClick = () => {
+      if (currentComment.trim() !== '') {
+          setComments(prevComments => [...prevComments, currentComment]);
+          setCurrentComment('');
+      }
+  }
 
-  handlePostClick = () => {
-    if (this.state.currentComment.trim() !== "") {
-      this.setState((prevState) => ({
-        comments: [...prevState.comments, prevState.currentComment],
-        currentComment: ""
-      }));
-    }
-  };
-
-  render() {
-    return (
+  return (
       <div>
-        <form>
-          <input
-            type="text"
-            value={this.state.currentComment}
-            onChange={this.handleInputChange}
-          />
-          <input type="button" value="Post" onClick={this.handlePostClick} />
-        </form>
-        <ul>
-          {this.state.comments.map((comment, index) => (
-            <li key={index}>{comment}</li>
-          ))}
-        </ul>
+          <form>
+              <input 
+                  type="text" 
+                  value={currentComment} 
+                  onChange={handleInputChange}
+              />
+              <input 
+                  type="button" 
+                  value="Post" 
+                  onClick={handlePostClick}
+              />
+          </form>
+          <ul>
+              {comments.map((comment, index) => (
+                  <li key={index}>{comment}</li>
+              ))}
+          </ul>
       </div>
-    );
-  }
+  );
 }
 
 document.body.innerHTML = "<div id='root'></div>";
@@ -55,10 +44,10 @@ root.render(<CommentList />);
 setTimeout(() => {
   let input = document.querySelector("input[type='text']");
   input.value = "test";
-  input.dispatchEvent(new Event("change", { bubbles: true }));
-
+  input.dispatchEvent(new Event('change', { bubbles: true }));
+  
   setTimeout(() => {
-    document.querySelector("input[type='button']").click();
-    console.log(document.getElementsByTagName("ul")[0].innerHTML);
+      document.querySelector("input[type='button']").click();
+      console.log(document.getElementsByTagName("ul")[0].innerHTML);
   });
 });
